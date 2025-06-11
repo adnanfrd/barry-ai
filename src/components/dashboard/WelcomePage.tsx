4737913c7e99d429c3e45ef6dcd27b1940b6a480
adnanfrd <adnanmadhar74@gmail.com>
2025-06-12 00:47:10 +0500

    updated

--- src/components/dashboard/WelcomePage.tsx
+++ src/components/dashboard/WelcomePage.tsx
@@ -15,36 +15,8 @@ export default function WelcomePage() {
         <div className="max-w-[90%] md:max-w-[75%] flex gap-20 mb-4 rounded-2xl border border-[#E2DFFC] overflow-hidden">
           <div className="flex-1 bg-white px-4 py-3">
             <p className="text-sm text-gray-800 mb-2">
-              Effect library connection image main. Rectangle star strikethrough
-              bullet fill. Ellipse slice overflow italic style align edit
-              inspect horizontal arrow.
+             Layer main distribute undo edit resizing export vertical. Export vertical move star plugin group scrolling figjam invite invite.
             </p>
-            <p className="text-sm text-gray-800 mb-2">
-              Ipsum main strikethrough variant object comment boolean move
-              background. Asset select list asset align underline auto bold
-              community main.
-            </p>
-            <div className="w-32 h-36 rounded-xl overflow-hidden relative">
-              <Image
-                src="/photo.png"
-                alt="photo"
-                width={128}
-                height={144}
-                className="rounded-xl object-cover w-full h-full"
-              />
-              <div className="absolute inset-0 flex items-center justify-center">
-                <div className="w-8 h-8 bg-white rounded-full flex items-center justify-center shadow-md">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-4 h-4 text-gray-800 ml-[2px]"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M6 4l10 6-10 6V4z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
           </div>
           <div className="w-14 bg-[#F4F1FF] flex flex-col items-center py-2 px-1">
             <Image
