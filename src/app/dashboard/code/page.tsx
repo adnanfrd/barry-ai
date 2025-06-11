6882658ce528ecb93b59c20188b71c33e5299581
adnanfrd <adnanmadhar74@gmail.com>
2025-06-11 12:36:58 +0500

    updated

--- src/app/dashboard/code/page.tsx
+++ src/app/dashboard/code/page.tsx
@@ -6,15 +6,12 @@ import React from 'react'
 const page = () => {
   return (
      <div className="flex h-screen">
-      {/* Left Side: Header + Code UI */}
       <div className="flex flex-col flex-1 overflow-hidden px-3">
         <Header />
         <div className="flex-1 ">
           <CodeChatUI />
         </div>
       </div>
-
-      {/* Right Side: Sidebar only, no header */}
       <div>
         <ChatSidebar />
       </div>
