39c8d4842fa834eecf99f2144c39f74d8ff3ea7d
adnanfrd <adnanmadhar74@gmail.com>
2025-06-13 23:57:16 +0500

    remove scrollbar lol

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${poppins.variable} dark`}>
-      <body className={`${poppins.className} bg-[#f9fafb] text-gray-900 dark:bg-[#1f1f1f] dark:text-white`}>
+      <body className={`${poppins.className} bg-[#f9fafb] overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] text-gray-900 dark:bg-[#1f1f1f] dark:text-white`}>
         <div className="flex min-h-screen px-2 py-2 rounded-xl bg-[#eae9ec] dark:bg-[#2c2c2e] w-full">
             <Dashboard  />
           <main className="flex-1 px-6 w-full">{children}</main>
