ec1574a7ba06b0bbf37d328b5259e09025f8b481
adnanfrd <adnanmadhar74@gmail.com>
2025-06-21 15:42:30 +0500

    updated code fix error

--- src/app/dashboard/settings/layout.tsx
+++ src/app/dashboard/settings/layout.tsx
@@ -1,30 +1,21 @@
 import Profiledashboard from "@/components/dashboard/profile/Profiledashboard";
 import type { Metadata } from "next";
-import { Poppins } from 'next/font/google';
 export const metadata: Metadata = {
   title: "Fusion-AI",
   description: "Fusion Ai theme and dashboard",
 };
 
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'], 
-  variable: '--font-poppins',
-  display: 'swap',
-})
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={poppins.variable}>
-      <body className={`${poppins.className} bg-[#f9fafb] text-gray-900`}>
+      <body className={` bg-[#f9fafb] text-gray-900`}>
          <div className="flex w-full"> 
           <Profiledashboard />
           <main className="flex-1 rounded-r-xl w-full">{children}</main> 
         </div>
       </body>
-    </html>
   );
 }
